6f869d2781e0cf91e842f823768734b4c25add23
werinton <werinton@MacBook-Air-de-Werinton.local>
2022-09-06 13:13:45 -0300

    codificada funcionalidade

--- node/index.js
+++ node/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 const config = {
     host: 'mysql',
     user: 'root',
@@ -11,11 +11,28 @@ const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
 const sql = `INSERT INTO people(name) values ('werinton')`
-connection.query(sql)
+connection.query(sql)  
 connection.end()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle </h1>')
+    let body = '<p><h1>Full Cycle Rocks!</h1></p>'
+    body += '<p>- Lista de nomes cadastrada no banco de dados.</p>'
+
+
+    let con = mysql.createConnection(config)
+    con.query("SELECT * FROM people", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+
+        Object.keys(result).forEach(function(key) {
+            var row = result[key];
+            body += row.name + '</br>'
+            console.log(row.name)
+          });
+        res.send(body);
+
+    });
+    con.end()
 })
 
 app.listen(port, () => {
